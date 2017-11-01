273b2644ae45956adc6ee67cf08d06e79f490175
Timbo Cole <timbo_cole@yahoo.co.uk>
2017-11-01 01:32:25 +0000

    Scrapy Tutorial

---
+++ scrapingTool/tutorial/tutorial/spiders/quotes_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for quote in response.css('div.quote') :
+            yield {
+                'title' : quote.css('span.text::text').extract_first(),
+                'author' : quote.css('small.author::text').extract_first(),
+                'tags' : quote.css('a.tag::text').extract()
+            }
